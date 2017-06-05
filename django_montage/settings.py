de0b326d44ebb0ab9f793c834ec09dac97d8e947
Matthew Wong <mttwong@vt.edu>
2017-06-05 17:28:06 -0400

    Adding a view to show users

--- django_montage/settings.py
+++ django_montage/settings.py
@@ -128,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'user:profile'
--- rafter_user_service/urls.py
+++ rafter_user_service/urls.py
@@ -1,13 +1,16 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rafter_user_service.views import ApplicationList, ApplicationCreate, \
-ApplicationDetail, get_token, authenticate_app, get_public_key
+ApplicationDetail, get_token, authenticate_app, get_public_key, UserDetail, \
+user_profile
 
 app_name = 'user'
 urlpatterns = [
+    url(r'^$', user_profile, name='profile'),
     url(r'^applications/$', ApplicationList.as_view(), name='apps'),
     url(r'^applications/create/$', ApplicationCreate.as_view(), name='create_app'),
     url(r'^applications/(?P<pk>[0-9]+)/$', ApplicationDetail.as_view(), name='detail_app'),
     url(r'^applications/(?P<pk>[0-9]+)/secret/$', get_token, name='secret'),
     url(r'^authenticate/(?P<pk>[0-9]+)/$', authenticate_app, name='auth_app'),
-    url(r'^public_key/$', get_public_key, name='pub_key')
+    url(r'^public_key/$', get_public_key, name='pub_key'),
+    url(r'^(?P<username>.+)/$', UserDetail.as_view(), name='user'),
 ]
