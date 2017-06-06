332c6ff3d13831ba176c81f7c1f1f2c6b5b48a57
Matthew Wong <mttwong@vt.edu>
2017-06-06 07:49:49 -0400

    Separating tests into test_*, and placing them in a test module.

---
+++ rafter_user_service/test/test_models.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from rafter_user_service.models import Application
+from django.contrib.auth.models import User
+from .util import make_user, make_test_user, make_app
+
+
+class ProfileTest(TestCase):
+    def test_make_user_makes_profile(self):
+        user = make_test_user()
+        self.assertIsNotNone(user.profile)
+        self.assertEqual(user.profile._roles, '[]')
+        self.assertEqual(user.profile._access, '["@core", "#public"]')
+
+    def test_roles(self):
+        user = make_test_user()
+        user.profile.roles = ['some', 'roles']
+        self.assertEqual(user.profile._roles, '["some", "roles"]')
+        self.assertEqual(user.profile.roles, ['some', 'roles'])
+
+    def test_access(self):
+        user = make_test_user()
+        user.profile.access = ['some', 'access']
+        self.assertEqual(user.profile._access, '["some", "access"]')
+        self.assertEqual(user.profile.access, ['some', 'access'])
+
+class ApplicationTest(TestCase):
+    def setUp(self):
+        self.user = make_test_user()
+        
+    def test_user(self):
+        Application.objects.create(user=self.user, name='test')
+
+    def test_user_user_is_none(self):
+        with self.assertRaises(TypeError):
+            Application.objects.create(user=None, name='test')
+
+    def test_user_user_is_str(self):
+        with self.assertRaises(TypeError):
+            Application.objects.create(user='oh no!', name='test')
+
+    def test_generate_access_token(self):
+        app = make_app(self.user, 'test')
+        token = app.generate_access_token()
+
+        self.assertIsNotNone(app.access_token)
+        self.assertNotEqual(len(app.access_token), 0)
+        self.assertTrue(app.check_token(token))
--- rafter_user_service/test/test_views.py
+++ rafter_user_service/test/test_views.py
@@ -2,57 +2,7 @@ from django.test import TestCase
 from rafter_user_service.models import Application
 from django.contrib.auth.models import User
 from django.urls import reverse
-
-def make_user(username):
-    return User.objects.create_user(username, 'test@test.com', 'test')
-
-def make_test_user():
-    return make_user('test')
-
-class ProfileTest(TestCase):
-    def test_make_user_makes_profile(self):
-        user = make_test_user()
-        self.assertIsNotNone(user.profile)
-        self.assertEqual(user.profile._roles, '[]')
-        self.assertEqual(user.profile._access, '["@core", "#public"]')
-
-    def test_roles(self):
-        user = make_test_user()
-        user.profile.roles = ['some', 'roles']
-        self.assertEqual(user.profile._roles, '["some", "roles"]')
-        self.assertEqual(user.profile.roles, ['some', 'roles'])
-
-    def test_access(self):
-        user = make_test_user()
-        user.profile.access = ['some', 'access']
-        self.assertEqual(user.profile._access, '["some", "access"]')
-        self.assertEqual(user.profile.access, ['some', 'access'])
-
-def make_app(user, name):
-    return Application.objects.create(user=user, name=name) 
-
-class ApplicationTest(TestCase):
-    def setUp(self):
-        self.user = make_test_user()
-        
-    def test_user(self):
-        Application.objects.create(user=self.user, name='test')
-
-    def test_user_user_is_none(self):
-        with self.assertRaises(TypeError):
-            Application.objects.create(user=None, name='test')
-
-    def test_user_user_is_str(self):
-        with self.assertRaises(TypeError):
-            Application.objects.create(user='oh no!', name='test')
-
-    def test_generate_access_token(self):
-        app = make_app(self.user, 'test')
-        token = app.generate_access_token()
-
-        self.assertIsNotNone(app.access_token)
-        self.assertNotEqual(len(app.access_token), 0)
-        self.assertTrue(app.check_token(token))
+from .util import make_user, make_test_user, make_app
 
 class ApplicationListViewTest(TestCase):
     def setUp(self):
