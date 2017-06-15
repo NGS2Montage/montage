3043b8ea38378a8e77bd6ab631b39c44c76b6cae
Brandon Potts <ObeliskIsHuge@gmail.com>
2017-06-15 17:30:12 -0400

    added packages for mongodb and python and added mongodb as a backend in settings

--- django_montage/settings.py
+++ django_montage/settings.py
@@ -110,8 +110,8 @@ WSGI_APPLICATION = 'django_montage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django_mongodb_engine',
+        'NAME': 'montage'
     }
 }
 
