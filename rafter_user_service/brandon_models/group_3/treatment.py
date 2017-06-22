9b24a5f880c6b152de052ac29f25a65d6b5f8485
Brandon Potts <ObeliskIsHuge@gmail.com>
2017-06-22 09:42:21 -0400

    implemented the rest of the Manipulation model

--- rafter_user_service/brandon_models/group_3/treatment.py
+++ rafter_user_service/brandon_models/group_3/treatment.py
@@ -1,9 +1,17 @@
 from django.db import models
+from .manipulation import Manipulation
+from .population import Population
 
 import datetime
 
 
 class Treatment(models.Model):
+    rid = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    manipulation = models.ManyToManyField(Manipulation)
+    population = models.ManyToManyField(Population)
+    is_active = models.CharField(max_length=200)  # TODO
     created_by = models.ForeignKey('rafter_user_service.investigator')
     modified_by = models.ForeignKey('rafter_user_service.investigator')
     created_on = models.CharField(datetime.date.today())
