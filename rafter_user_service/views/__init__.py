79ab5e4c474422e88dca0c7fec53eff2df84253d
Brandon Potts <ObeliskIsHuge@gmail.com>
2017-07-31 16:39:56 -0400

    made rafter_user_service a folder instead of just a file

---
+++ rafter_user_service/views/__init__.py
@@ -0,0 +1,6 @@
+# File used to import all the views in the folder
+# This is required for Django to keep track of all
+# the models.
+
+from .permissions import UserDetail, user_profile, ApplicationList, \
+    ApplicationDetail, ApplicationCreate, ApplicationJWT, get_token, get_public_key
