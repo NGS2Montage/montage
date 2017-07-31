c1f7ff365b57d61ff6565dcf2a671fff81ac1b7f
Brandon Potts <ObeliskIsHuge@gmail.com>
2017-07-31 15:03:59 -0400

    added primary keys to serialization functions

--- rafter_user_service/serializers.py
+++ rafter_user_service/serializers.py
@@ -35,128 +35,154 @@ class AnalysisSerializer(serializers.HyperlinkedModelSerializer):
 class AnalysisInstanceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = AnalysisInstance
+        fields = 'id'
 
 
 class ConfirmatoryLoopSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ConfirmatoryLoop
+        fields = 'id'
 
 
 class ExperimentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Experiment
+        fields = ('id', 'name')
 
 
 class ExperimentInstanceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ExperimentInstance
+        fields = ('id', 'experiment', 'experiment_status')
 
 
 class ExperimentStatusSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = AnalysisInstance
+        model = ExperimentStatus
+        fields = ('id', 'name')
 
 
 class ExperimentLoopSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ExploratoryLoop
+        fields = 'id'
 
 
 class InputSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Input
+        fields = ('id', 'name')
 
 
 class InvestigationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Investigation
+        fields = ('id', 'name', 'project')
 
 
 class InvestigationStatusSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = InvestigationStatus
+        fields = ('id', 'name')
 
 
 class InvestigatorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Investigator
+        fields = ('id', 'name', 'user_name')
 
 
 class LiteratureSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Literature
+        fields = ('id', 'name', 'author')
 
 
 class ManipulationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Manipulation
+        fields = ('id', 'name')
 
 
 class ModelSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Model
+        fields = ('id', 'name')
 
 
 class ModelInstanceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ModelInstance
+        fields = 'id'
 
 
 class ModelStatusSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ModelStatus
+        fields = ('id', 'name')
 
 
 class OutputSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Output
+        fields = ('id', 'name','type')
 
 
 class ParameterSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Parameter
+        fields = ('id', 'name','type')
 
 
 class PopulationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Population
+        fields = ('id', 'name')
 
 
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Profile
+        fields = ('id', 'user')
 
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Project
+        fields = ('id', 'team', 'project_state')
 
 
 class ProjectStateSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ProjectState
+        fields = ('id', 'name')
 
 
 class RoleSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Role
+        fields = ('id', 'name')
 
 
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Team
+        fields = ('id', 'name')
 
 
 class TheorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Theory
+        fields = ('id', 'name')
 
 
 class TheoryInstanceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = TheoryInstance
+        fields = ('id', 'name', 'model')
 
 
 class TreatmentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Treatment
+        fields = ('id', 'name')
