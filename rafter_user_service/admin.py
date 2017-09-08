88da8c4f3759a1be5a0ffa804afebce74dcd7d02
Nathan Self <nwself@cs.vt.edu>
2017-09-08 10:56:46 -0400

    Add a few models to admin interface

--- rafter_user_service/admin.py
+++ rafter_user_service/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from simple_history.admin import SimpleHistoryAdmin
+
+from .models import ProjectState, Investigator, Observation, Team
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Observation)
+class ObservationAdmin(SimpleHistoryAdmin):
+    pass
+
+@admin.register(Investigator)
+class InvestigatorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProjectState)
+class ProjectStateAdmin(admin.ModelAdmin):
+    pass
