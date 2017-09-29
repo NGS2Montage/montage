bcab8b644e3e7437d59988c809f873f68a717c98
Joshua Sherman <joshua.v.sherman@gmail.com>
2017-09-29 07:48:31 -0400

    code quality linters and atom config

--- montage_ui/static/js/montage/model/ThresholdModel.js
+++ montage_ui/static/js/montage/model/ThresholdModel.js
@@ -1,80 +1,80 @@
 define([
-        "dojo/_base/declare", "../form/base", "dojo/on","dojo/dom-construct",
-        "dojo/dom-class","dojo/text!./template/ThresholdModel.html",
-        "dojo/request","dijit/form/ValidationTextBox","dojo/topic","dojo/_base/lang",
-		"dijit/form/CheckBox","dijit/form/Select","dijit/form/Button","dojo/query",
-		"dojo/when","dojo/dom-construct"
+  "dojo/_base/declare", "../form/base", "dojo/on","dojo/dom-construct",
+  "dojo/dom-class","dojo/text!./template/ThresholdModel.html",
+  "dojo/request","dijit/form/ValidationTextBox","dojo/topic","dojo/_base/lang",
+  "dijit/form/CheckBox","dijit/form/Select","dijit/form/Button","dojo/query",
+  "dojo/when","dojo/dom-construct"
 ], function(
-	declare, FormBase, on, domConstruct,
-	domClass, Template,
-	Request,ValidationTextBox, Topic,lang,
-	CheckBox,Select,Button,Query,
-	when,domConstruct
+  declare, FormBase, on, domConstruct,
+  domClass, Template,
+  Request,ValidationTextBox, Topic,lang,
+  CheckBox,Select,Button,Query,
+  when,domConstruct
 
 
 ){
 
-	return declare([FormBase], {
-		templateString: Template,
-		model: null,
-		_setModelAttr: function(val){
-			this.model = val;
-			if (this.model.params){
-				this.setValues(this.model.params);
-			}else{
+  return declare([FormBase], {
+    templateString: Template,
+    model: null,
+    _setModelAttr: function(val){
+      this.model = val;
+      if (this.model.params){
+        this.setValues(this.model.params);
+      }else{
 
-			}
-		},
-		constructor: function(){
-			this.store = window.App.store.observation;
-		},
-
-
-
-		startup: function(){
-			if (this._started) { return; }
-			this.inherited(arguments);
-			this.validate();
-
-		},
+      }
+    },
+    constructor: function(){
+      this.store = window.App.store.observation;
+    },
 
 
-        onSubmit: function(evt){
-			var _self = this;
 
-			evt.preventDefault();
-			evt.stopPropagation();
+    startup: function(){
+      if (this._started) { return; }
+      this.inherited(arguments);
+      this.validate();
 
-			if(this.validate()){
-
-			        var values = this.getValues();
-			        this.showMessage("working");
-			        console.log("Form is Valid!", values)
-
-			        this.model.params = values;;
+    },
 
 
-		    		on.emit(this.domNode, "UpdateModel", {
-						   bubbles: true,
-						   cancelable: true,
-						   model: this.model
-					})
+    onSubmit: function(evt){
+      var _self = this;
 
-					on.emit(this.domNode, "DialogAction", {
-					    bubbles: true,
-		    			cancelable: true,
-						action: "close"
-					});
+      evt.preventDefault();
+      evt.stopPropagation();
 
-			}else{
-			        console.log("Form is incomplete");
-			}
-		},
-		onCancel: function(){
-			console.log("Add Observation onCancel()");
-			this.inherited(arguments);
+      if(this.validate()){
 
-		}
-	});
+        var values = this.getValues();
+        this.showMessage("working");
+        console.log("Form is Valid!", values)
+
+        this.model.params = values;;
+
+
+        on.emit(this.domNode, "UpdateModel", {
+          bubbles: true,
+          cancelable: true,
+          model: this.model
+        })
+
+        on.emit(this.domNode, "DialogAction", {
+          bubbles: true,
+          cancelable: true,
+          action: "close"
+        });
+
+      }else{
+        console.log("Form is incomplete");
+      }
+    },
+    onCancel: function(){
+      console.log("Add Observation onCancel()");
+      this.inherited(arguments);
+
+    }
+  });
 
 });
--- montage_ui/static/js/montage/model/WordGameModel.js
+++ montage_ui/static/js/montage/model/WordGameModel.js
@@ -1,81 +1,81 @@
 define([
-        "dojo/_base/declare", "../form/base", "dojo/on","dojo/dom-construct",
-        "dojo/dom-class","dojo/text!./template/WordGameModel.html",
-        "dojo/request","dijit/form/ValidationTextBox","dojo/topic","dojo/_base/lang",
-		"dijit/form/CheckBox","dijit/form/Select","dijit/form/Button","dojo/query",
-		"dojo/when","dojo/dom-construct"
+  "dojo/_base/declare", "../form/base", "dojo/on","dojo/dom-construct",
+  "dojo/dom-class","dojo/text!./template/WordGameModel.html",
+  "dojo/request","dijit/form/ValidationTextBox","dojo/topic","dojo/_base/lang",
+  "dijit/form/CheckBox","dijit/form/Select","dijit/form/Button","dojo/query",
+  "dojo/when","dojo/dom-construct"
 ], function(
-	declare, FormBase, on, domConstruct,
-	domClass, Template,
-	Request,ValidationTextBox, Topic,lang,
-	CheckBox,Select,Button,Query,
-	when,domConstruct
+  declare, FormBase, on, domConstruct,
+  domClass, Template,
+  Request,ValidationTextBox, Topic,lang,
+  CheckBox,Select,Button,Query,
+  when,domConstruct
 
 
 ){
 
-	return declare([FormBase], {
-		templateString: Template,
-		model: null,
+  return declare([FormBase], {
+    templateString: Template,
+    model: null,
     entropySource:"",
-		_setModelAttr: function(val){
-			this.model = val;
-			if (this.model.params){
-				this.setValues(this.model.params);
-			}else{
+    _setModelAttr: function(val){
+      this.model = val;
+      if (this.model.params){
+        this.setValues(this.model.params);
+      }else{
 
-			}
-		},
-		constructor: function(){
-			this.store = window.App.store.observation;
-		},
+      }
+    },
+    constructor: function(){
+      this.store = window.App.store.observation;
+    },
 
-//TODO on activate get random seed and set this.entropySource =
+    //TODO on activate get random seed and set this.entropySource =
 
-		startup: function(){
-			if (this._started) { return; }
-			this.inherited(arguments);
-			this.validate();
+    startup: function(){
+      if (this._started) { return; }
+      this.inherited(arguments);
+      this.validate();
 
-		},
-
-
-        onSubmit: function(evt){
-			var _self = this;
-
-			evt.preventDefault();
-			evt.stopPropagation();
-
-			if(this.validate()){
-
-			        var values = this.getValues();
-			        this.showMessage("working");
-			        console.log("Form is Valid!", values)
-
-			        this.model.params = values;;
+    },
 
 
-		    		on.emit(this.domNode, "UpdateModel", {
-						   bubbles: true,
-						   cancelable: true,
-						   model: this.model
-					})
+    onSubmit: function(evt){
+      var _self = this;
 
-					on.emit(this.domNode, "DialogAction", {
-					    bubbles: true,
-		    			cancelable: true,
-						action: "close"
-					});
+      evt.preventDefault();
+      evt.stopPropagation();
 
-			}else{
-			        console.log("Form is incomplete");
-			}
-		},
-		onCancel: function(){
-			console.log("Add Observation onCancel()");
-			this.inherited(arguments);
+      if(this.validate()){
 
-		}
-	});
+        var values = this.getValues();
+        this.showMessage("working");
+        console.log("Form is Valid!", values)
+
+        this.model.params = values;;
+
+
+        on.emit(this.domNode, "UpdateModel", {
+          bubbles: true,
+          cancelable: true,
+          model: this.model
+        })
+
+        on.emit(this.domNode, "DialogAction", {
+          bubbles: true,
+          cancelable: true,
+          action: "close"
+        });
+
+      }else{
+        console.log("Form is incomplete");
+      }
+    },
+    onCancel: function(){
+      console.log("Add Observation onCancel()");
+      this.inherited(arguments);
+
+    }
+  });
 
 });
