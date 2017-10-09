620df2795c18dd87e5cfdb0d26ee53cf2d22a0db
Joshua Sherman <joshua.v.sherman@gmail.com>
2017-10-09 16:13:38 -0400

    still working on getting the projects

--- montage_ui/static/js/montage/project/viewer.js
+++ montage_ui/static/js/montage/project/viewer.js
@@ -1,167 +1,158 @@
 define([
-        "dojo/_base/declare", "dijit/layout/BorderContainer","dijit/layout/ContentPane",
-	"dojo/when","dojo/_base/lang","dojo/string","dojo/text!./template/ProjectOverviewPanel.html",
-	"dijit/layout/TabContainer","dojo/query","dojo/on","dojo/dom-class","dojo/dom-attr","./investigation",
-	"../form/observation","../widget/Dialog","./observations","../widget/Toolbar","dojo/dom-construct"
+  "dojo/_base/declare", "dijit/layout/BorderContainer","dijit/layout/ContentPane",
+  "dojo/when","dojo/_base/lang","dojo/string","dojo/text!./template/ProjectOverviewPanel.html",
+  "dijit/layout/TabContainer","dojo/query","dojo/on","dojo/dom-class","dojo/dom-attr","./investigation",
+  "../form/observation","../widget/Dialog","./observations","../widget/Toolbar","dojo/dom-construct"
 ], function(
-	declare,BorderContainer,ContentPane,
-	when,lang,dojoString,POPTemplate,
-	TabContainer,Query,on,domClass,domAttr,Investigation,
-	Observation,Dialog,Observations,Toolbar,domConstruct
+  declare,BorderContainer,ContentPane,
+  when,lang,dojoString,POPTemplate,
+  TabContainer,Query,on,domClass,domAttr,Investigation,
+  Observation,Dialog,Observations,Toolbar,domConstruct
 ){
+  return declare([BorderContainer], {
+    state: null,
+    project: null,
+    investigations: null,
+    currentInvestigation: null,
+    projectStore: window.App.store.project,
+    investigationStore: window.App.store.investigation,
+    _setStateAttr: function(state){
+      this.state = state;
+      console.log("Project Viewer setState: ", state);
+      if (state.project_id){
+        this.getProject(state.project_id);
+      }
+    },
+    getProject: function(id){
+      console.log("getProject()");
+      return when(this.projectStore.get(id), lang.hitch(this,function(project){
+        console.log("Project: ", project);
+        this.set("project", project);
+      }));
+    },
 
-        return declare([BorderContainer], {
-		state: null,
-		project: null,
-		investigations: null,
-		currentInvestigation: null,
-		projectStore: window.App.store.project,
-		investigationStore: window.App.store.investigation,
-		_setStateAttr: function(state){
-			this.state=state;
-			console.log("Project Viewer setState: ", state);
-			if (state.project_id){
-				this.getProject(state.project_id);
-			}
-		},
+    _setProjectAttr: function(project){
+      this.project = project;
+      console.log('inside viewer js');
+      console.log(this.project);
+      if (this._started){
+        this.projectHeader.set("content",dojoString.substitute(POPTemplate,project));
+        this.observationsView.set("projectId", project.id);
+        this.investigationToolbar.enable("addObservation");
+      }
 
-		getProject: function(id){
-			console.log("getProject()");
-			return when(this.projectStore.get(id), lang.hitch(this,function(project){
-				console.log("Project: ", project);
-				this.set("project", project);		
-			}));
-		},
+      this.getInvestigations();
+    },
 
-		_setProjectAttr: function(project){
-			this.project=project;
-			if (this._started){
-				this.projectHeader.set("content",dojoString.substitute(POPTemplate,project));
-				this.observationsView.set("projectId", project.id);
-				this.investigationToolbar.enable("addObservation")
-			}
+    _setInvestigationsAttr: function(investigations){
+      this.investigations = investigations;
+      var out = [];
+      console.log("set investigations: ", investigations);
+      if (this._started){
+        console.log("Render Investigations List", investigations);
+        investigations.forEach(function(investigation,index){
+          var classes = (investigation.id === this.currentInvestigation.id) ? "SelectedInvestigation" : "";
+          out.push('<div rel="' + index + '"  class="InvestigationListButton ' + classes + '"><span>' + investigation.id + "</span>" + '&nbsp;<span>' + investigation.status + '<br>' + investigation.lastModified + '</div>');
+        },this);
+        console.log("Set InvestigationsList: ", out.join(""));
+        this.investigationsList.set("content", out.join(""));
+      }
+    },
 
-			this.getInvestigations();
-		},
+    _setCurrentInvestigationAttr: function(investigation){
+      this.currentInvestigation = investigation;
+      if (this._started){
+        this.investigationView.set("investigation",investigation);
+      }
+    },
 
-		_setInvestigationsAttr: function(investigations){
-			this.investigations = investigations;
-			console.log("set investigations: ", investigations);
-			if (this._started){
-				console.log("Render Investigations List", investigations);
-				var out=[]
-				investigations.forEach(function(investigation,index){
-					var classes= (investigation.id == this.currentInvestigation.id)?"SelectedInvestigation":"";
-					out.push('<div rel="' +index+ '"  class="InvestigationListButton ' +classes + '"><span>' + investigation.id +"</span>" + '&nbsp;<span>' + investigation.status+ '<br>' + investigation.lastModified + '</div>');
-				},this);
-				console.log("Set InvestigationsList: ", out.join(""));
-				this.investigationsList.set("content", out.join(""));
-			}
-		},
+    getInvestigations: function(){
+      return when(this.investigationStore.query({project: this.project.id},{sort: [{attribute: "createdOn",descending: true}]}), lang.hitch(this,function(results){
+        if (results.length > 0) {
+          this.set("currentInvestigation", results[0]);
+          this.set("investigations", results);
+        } else {
+          return when(this.investigationStore.add({project: this.project.id}), lang.hitch(this,function(I){
+            console.log("Investigation I: ", I);
+            return when(this.investigationStore.get(I), lang.hitch(this,function(inv){
+              this.set("currentInvestigation", inv);
+              this.set("investigations", [inv]);
+            }));
+          }));
+        }
+      }));
+    },
 
-		_setCurrentInvestigationAttr: function(investigation){
-			this.currentInvestigation = investigation;
-			if (this._started){
-				this.investigationView.set("investigation",investigation) 
-			}
-	
-		},
+    showDialog: function(widget, title){
+      var dlg = new Dialog({title: title});
 
-		getInvestigations: function(){
-			return when(this.investigationStore.query({project: this.project.id},{sort: [{attribute: "createdOn",descending: true}]}), lang.hitch(this,function(results){
-				if (results.length>0) {
-					this.set("currentInvestigation", results[0]);
-					this.set("investigations", results);
-				}else{
-					return when(this.investigationStore.add({project: this.project.id}), lang.hitch(this,function(I){
-						console.log("Investigation I: ", I)
-						return when(this.investigationStore.get(I), lang.hitch(this,function(inv){
-							this.set("currentInvestigation", inv)
-							this.set("investigations", [inv]);
-						}))
-					}))
-				}
-			}));	
-		},
+      domConstruct.empty(dlg.containerNode);
+      domConstruct.place(widget.domNode,dlg.containerNode,"first");
+      widget.startup();
+      dlg.show();
+    },
 
-		showDialog: function(widget, title){
-			var dlg = new Dialog({title: title});
+    startup: function(){
+      var headerContent = (this.project && this.project.name) ? dojoString.substitute(POPTemplate,this.project) : "";
+      //	var headerContent = "Header Content";
+      this.projectHeader = new ContentPane({content: headerContent, region: "top"});
+      //var investigationContent = (this.investigations && this.investigations[0]) ? this.investigations[0] : {};
 
-			domConstruct.empty(dlg.containerNode);
-			domConstruct.place(widget.domNode,dlg.containerNode,"first");
-			widget.startup();
-			dlg.show()
-		},
-		
-		startup: function(){
-			var headerContent = (this.project&&this.project.name)?dojoString.substitute(POPTemplate,this.project):""	
-		//	var headerContent = "Header Content";
-			this.projectHeader = new ContentPane({content: headerContent, region: "top"});
-	
+      var tb = this.investigationToolbar = new Toolbar({region: "top", style: "border: 0px;margin:0px;padding:2px;"});
 
-		
-			var investigationContent = (this.investigations && this.investigations[0])?this.investigations[0]:{};	
+      tb.addAction("addObservation", {
+        icon: "icon fa-2x icon-comment",
+        disabled: true,
+        action: lang.hitch(this,function(){
+          var w = new Observation({projectId: this.project.id});
+          this.showDialog(w,"Add Observation");
+        })
+      });
 
-			var tb = this.investigationToolbar = new Toolbar({region: "top", style: "border: 0px;margin:0px;padding:2px;"});
+      tb.addAction("play", {
+        icon: "icon fa-2x icon-play",
+        disabled: true,
+        action: function(){
+          console.log("Play");
+        }
+      });
 
-			tb.addAction("addObservation", {
-				icon: "icon fa-2x icon-comment",
-				disabled: true,
-				action: lang.hitch(this,function(){
-					var w = new Observation({projectId: this.project.id})
-					this.showDialog(w,"Add Observation");
-				})
-			})
+      this.investigationView = new Investigation({region: "center",investigation: this.investigation ? this.investigation : null });
+      this.addChild(this.projectHeader);
+      this.addChild(this.investigationToolbar);
+      this.addChild(this.investigationView);
+      this.sidePanelTabContainer = new TabContainer({style: "width: 200px;",region: "right", splitter: true});
+      this.investigationsList = new ContentPane({title: "Investigations", region: "right", class: "InvestigationsList"});
+      this.sidePanelTabContainer.addChild(this.investigationsList);
+      this.observationsView = new Observations({style: "margin:0px;padding:0px;border:0px;", title: "Observations", projectId: this.project ? this.project.id : "", region: "right" });
+      this.sidePanelTabContainer.addChild(this.observationsView);
+      this.addChild(this.sidePanelTabContainer);
 
-			tb.addAction("play", {
-				icon: "icon fa-2x icon-play",
-				disabled: true,
-				action: function(){
-					console.log("Play");
-				}
-			})
+      console.log("Check for Investigations: ", this.investigations);
 
+      this.inherited(arguments);
 
-			this.investigationView = new Investigation({region: "center",investigation: this.investigation?this.investigation:null });
+      on(this.investigationsList.containerNode, ".InvestigationListButton:click", lang.hitch(this, function(evt){
+        console.log("evt.target", evt.target);
+        var target = evt.target;
+        while (!domClass.contains(target,"InvestigationListButton")) {
+          target = target.parentNode;
+        }
+        var idx = domAttr.get(target,"rel");
+        var investigation = this.investigations[idx];
+        this.set("currentInvestigation", investigation);
+        Query(".InvestigationListButton",this.investigationsList.containerNode).forEach(function(node){
+          domClass.remove(node, "SelectedInvestigation");
+        });
+        domClass.add(target, "SelectedInvestigation");
+      }));
 
-			this.addChild(this.projectHeader);
-			this.addChild(this.investigationToolbar);
-			this.addChild(this.investigationView);
-			this.sidePanelTabContainer = new TabContainer({style: "width: 200px;",region: "right", splitter:true});
-			this.investigationsList = new ContentPane({title: "Investigations", region: "right", class:"InvestigationsList"});
-			this.sidePanelTabContainer.addChild(this.investigationsList);
-			this.observationsView= new Observations({style: "margin:0px;padding:0px;border:0px;", title: "Observations", projectId: this.project?this.project.id:"", region: "right" });
-			this.sidePanelTabContainer.addChild(this.observationsView);
-			this.addChild(this.sidePanelTabContainer);
-
-			console.log("Check for Investigations: ", this.investigations);
-
-			this.inherited(arguments);
-
-			on(this.investigationsList.containerNode, ".InvestigationListButton:click", lang.hitch(this, function(evt){
-				console.log("evt.target", evt.target);
-				var target = evt.target;
-				while (!domClass.contains(target,"InvestigationListButton")) {
-					target=target.parentNode;
-				}
-				var idx = domAttr.get(target,"rel");
-				var investigation = this.investigations[idx];
-				this.set("currentInvestigation", investigation);
-				Query(".InvestigationListButton",this.investigationsList.containerNode).forEach(function(node){
-					domClass.remove(node, "SelectedInvestigation")
-				});
-				domClass.add(target, "SelectedInvestigation");
-			
-			}))
-
-
-
-			if (this.investigations) {
-				console.log("Found Investigations: ", this.investigations.length);	
-				this.set("investigations", this.investigations);
-				this.set("currentInvestigation", this.investigations[0]);
-				this.investigationToolbar.enable("addObservation")
-			}
-		}
-	})
+      if (this.investigations) {
+        console.log("Found Investigations: ", this.investigations.length);
+        this.set("investigations", this.investigations);
+        this.set("currentInvestigation", this.investigations[0]);
+        this.investigationToolbar.enable("addObservation");
+      }
+    }
+  });
 });
